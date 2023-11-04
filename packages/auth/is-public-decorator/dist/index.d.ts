efbac4a16cc14b6bba52d94214171f5db4abd35b
anton <anton.nagornyi@gmail.com>
2023-11-04 11:24:14 +0200

    fix: remove and ignore dist

--- packages/auth/is-public-decorator/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import * as _nestjs_common from '@nestjs/common';
-
-declare const IsPublicMetaKey = "isPublic";
-declare const IsPublic: () => _nestjs_common.CustomDecorator<string>;
-
-export { IsPublic, IsPublicMetaKey };
--- packages/auth/is-public-decorator/dist/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var common = require('@nestjs/common');
-
-const IsPublicMetaKey = 'isPublic';
-const IsPublic = () => common.SetMetadata('IsPublicMetaKey', true);
-
-exports.IsPublic = IsPublic;
-exports.IsPublicMetaKey = IsPublicMetaKey;
